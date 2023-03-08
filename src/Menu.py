d68c6649338191ea4fefc3fdbacd1ba4b031a2de
GersonOtoniel <69872823+GersonOtoniel@users.noreply.github.com>
2023-03-07 23:05:25 -0600

    Se agrego soluciones a errores de datos

--- src/Menu.py
+++ src/Menu.py
@@ -1,4 +1,5 @@
 from os import system
+import os
 from tkinter.filedialog import askopenfilename
 from xml.dom import minidom
 from ListaSimple import ListaSimple
@@ -49,13 +50,18 @@ class Menu:
         self.codigo = codigo[0].childNodes[0].data
 
         #Aquí se agrega la fila y la columna de cada celula como coordenadas x-y al igual que su codigo
+        if int(self.fila) > 10000:
+            print("Número de filas excede el rango alcanzado")
+        if int(self.columna) > 10000:
+            print("Numero de columnas excede el rango alcanzado") 
+
         self.nuevo = ListaSimple()
         for i in range(len(celdaviva)):
             codigoviva = celdaviva[i].childNodes[5].firstChild.data
             xviva = celdaviva[i].childNodes[3].firstChild.data
             yviva = celdaviva[i].childNodes[1].firstChild.data
             self.nuevo.insertar(codigoviva, xviva, yviva)
-        self.nuevo.recorrerLista()
+        #self.nuevo.recorrerLista()
 
         #Aquí se agrega el nombre y el codigo de los organismos en una lista simple
         letra = 65
@@ -66,12 +72,15 @@ class Menu:
             self.organismo.insertar(nombre, codigo, chr(letra))
             letra+=1
 
-        self.organismo.recorrerLista()    
+        #self.organismo.recorrerLista()
+
+         
         
 
     def creargrafica(self):
         #filax = self.fila 
         self.graph = """ digraph grid {
+        graph[label="Muestras de Marte"]
         fontname="Helvetica,Arial,sans-serif"
         node [fontname="Helvetica,Arial,sans-serif"]
         edge [fontname="Helvetica,Arial,sans-serif"]
@@ -121,41 +130,71 @@ class Menu:
                 
         
         self.graph+="\""            
-        self.graph+="]"
+        self.graph+="];"
+
+        self.graph+="\""
+        actual22 = self.organismo.primero
+        while actual22 != None:
+            organismo = actual22.codigo
+            letras = actual22.letra
+            #print(letras)
+            self.graph+= letras + "=" + organismo +"\n"
+            #self.graph+=f'''{actual22.letra} = {actual22.codigo}'''
+            actual22 = actual22.siguiente
+        self.graph+="\""
+        
         self.graph+="\n}"
          
 
-        archivo = open('./src/ejemplograph.dot','w')
-        archivo.write(self.graph)
+        #archivo = open('./src/ejemplograph.dot','w')
+        #archivo.write(self.graph)
+        #system('dot -Tpdf ./src/ejemplograph.dot -o ./src/ejemplograph.pdf')
 
-       # system('dot -Tpdf ./src/ejemplograph.dot -o ./src/grafica.pdf')
-        system('dot -Tpdf ./src/ejemplograph.dot -o ./src/ejemplograph.pdf')
-        #system('cd ./grafica.png')
+        with open('./src/ejemplograph.dot', 'w') as f:
+            f.write(self.graph)
+
+        # Aqui creamos la imagen
+        os.system('dot -Tpdf ./src/ejemplograph.dot -o ejemplo_graphviz.pdf')
+       
 
 
     def insertarcelula(self):
 
         count =0
-
+        contador=0
         while True:
             if count == 0:
-                self.fila = input("Inserte numero de fila: ")
+                self.fila1 = input("Inserte numero de fila: ")
                 count+=1
             if count == 1:
-                self.columna = input("Inserte numero de columna: ")
+                self.columna1 = input("Inserte numero de columna: ")
                 count+=1
             if count == 2:
                 self.codigo2 = input("Ingrese codigo del organismo: ")
                 count+=1
-            self.nuevo.insertar(self.codigo2, self.columna, self.fila)
-            if count==3:
+            if count == 3:
+                actual = self.nuevo.primero
+                while actual != None:
+                    if int(self.fila1) == int(actual.y) and int(self.columna1) == int(actual.x):
+                        contador+=1
+                    actual = actual.siguiente
+                if contador == 0:
+                    self.nuevo.insertar(self.codigo2, self.columna1, self.fila1)
+                    break
+                if contador > 0:
+                    system("cls")
+                    print("Ésta posición ya esta siendo usada por otra célula, por favor introduzca de nuevo los datos")
+                    self.insertarcelula()
+                count+=1
+            if count==4:
                 self.nuevo.recorrerLista()
                 count+=1
-            if count == 4:
+            if count == 5:
                 entrada = input("Ingrese 5 para salir: ")
             if int(entrada) == 5:
                 system("cls")
                 break
+            
 
             
 
@@ -170,42 +209,7 @@ class Menu:
                     actual = actual.siguiente
     """   
 
-    """
-    def posicionvalida(self):
-        
-        count = 0
-        count2 = 0
-        count3 = 0
-        for i in range(int(self.fila)):
-            for j in range(int(self.columna)):
-                actual = self.nuevo.primero
-                while actual != None:
-                    nodoactual = actual
-                    if int(actual.y)==i and int(actual.x)== j:
-                        if actual.dato == "SCP096":
-                            count+=1
-                        if actual.dato != "SCP096":
-                            count2+=1
-                        if count == 2:
-                            while actual != None:
-                                if actual.dato == "SCP096":
-                                    count3+=1
-                            
-                                if count3 == 2:
-                                    break
-                                if actual.dato != "SCP096":
-                                    actual.dato = "SCP096"
-                                actual = actual.anterior    
-                            actual = nodoactual     
-
-                    actual = actual.siguiente
-                
-                            
-        self.nuevo.recorrerLista()
-        """
-
-
-
+    """"""
     def jugadavalida(self):
         self.px = int(self.columna)
         self.py = int(self.fila)
@@ -217,12 +221,8 @@ class Menu:
         while actual != None:
             guardarnodo = actual
             if int(actual.x) == self.px and int(actual.y) == self.py:
-                if actual.dato != self.codigo2:
-                    continue
-                self.px+=1
                 if self.nuevo.indexsiguiente(self.px) != self.codigo2:
-                    self.px+=1
-                    if self.nuevo.indexsiguiente(self.px) == self.codigo2:
+                    if self.nuevo.indexsiguiente(self.px+1) == self.codigo2:
                         actual = actual.siguiente
                     actual.dato = self.codigo2
             actual = guardarnodo
